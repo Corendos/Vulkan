d60f96bdd7d7c4e3130bedf6d189fe813feaa391
godeau_cor <corentin.godeau@supelec.fr>
2018-09-29 23:06:29 +0200

    feat: open window

--- initiation/src/main.cpp
+++ initiation/src/main.cpp
@@ -1,35 +1,62 @@
+#include <vulkan/vulkan.h>
+
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
 #include <iostream>
+#include <stdexcept>
+#include <functional>
+#include <cstdlib>
+
+class HelloTriangleApplication {
+    public:
+        void run() {
+            initWindow();
+            initVulkan();
+            mainLoop();
+            cleanup();
+        }
+
+    private:
+        const int WIDTH = 800;
+        const int HEIGHT = 600;
+
+        GLFWwindow* mWindow;
+
+        void initWindow() {
+            glfwInit();
+
+            glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+            glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+            mWindow = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan API", nullptr, nullptr);
+        }
+
+        void initVulkan() {
+
+        }
+
+        void mainLoop() {
+            while(!glfwWindowShouldClose(mWindow)) {
+                glfwPollEvents();
+            }
+        }
+
+        void cleanup() {
+            glfwDestroyWindow(mWindow);
+            glfwTerminate();
+        }
+};
 
 int main() {
-    glfwInit();
+    HelloTriangleApplication app;
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::cout << extensionCount << " extensions supported" << std::endl;
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+    try {
+        app.run();
+    } catch(const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
     }
 
-    glfwDestroyWindow(window);
-
-    glfwTerminate();
-
-    return 0;
+    return EXIT_SUCCESS;
 }
