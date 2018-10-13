b86ac68423231a9e298a09d411861a9c9a80d4e2
godeau_cor <corentin.godeau@supelec.fr>
2018-10-13 20:19:50 +0200

    wip: memory management debug page print

--- initiation/src/memory/MemoryManager.cpp
+++ initiation/src/memory/MemoryManager.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 #include "memory/MemoryManager.hpp"
 
@@ -27,6 +28,17 @@ void MemoryManager::printInfo() {
             << "    " << freeBlock << " free blocks" << std::endl
             << "    " << occupiedBlock << " occupied blocks" << std::endl;
     }
+
+    std::ofstream file;
+    file.open("./memory.log", std::ios::out | std::ios::trunc);
+    for (size_t i{0};i < mMemoryProperties.memoryHeapCount;++i) {
+        file << "Memory Heap #" << i << std::endl;
+        for (size_t blockIndex{0};blockIndex < mMemoryOccupations[i].size();++blockIndex) {
+            file << mMemoryOccupations[i][blockIndex] ? "1" : "0";
+        }
+        file << std::endl;
+    }
+    file.close();
 }
 
 void MemoryManager::initialAllocation() {
@@ -71,10 +83,35 @@ void MemoryManager::allocateForBuffer(VkBuffer& buffer, VkMemoryRequirements& me
     std::cout   << "Required size: " << memoryRequirements.size << std::endl
                 << "Block count: " << getBlockCount(memoryRequirements.size) << std::endl;
 
+    uint32_t blockCount = getBlockCount(memoryRequirements.size);
+    uint32_t memoryHeapIndex = mMemoryProperties.memoryTypes[memoryTypeIndex].heapIndex;
+    
+    size_t i = 0;
+    size_t offset;
+    bool found = false;
+    while(i < mMemoryOccupations[memoryHeapIndex].size()) {
+        offset = i;
+        size_t count = 0;
+        while (count < blockCount && !mMemoryOccupations[memoryHeapIndex][i]) {
+            count++;
+            i++;
+        }
 
-    /*if (!(memoryRequirements.memoryTypeBits & (1 << mMemoryTypeIndex))) {
-        throw std::runtime_error("Memory type is not avalaible");
-    }*/
+        if (count == blockCount) {
+            found = true;
+            break;
+        }
+        i++;
+    }
+
+    if (!found) {
+        throw std::runtime_error("Failed to find free blocks");
+    }
+
+    for (size_t i{offset};i < offset + blockCount;++i) {
+        mMemoryOccupations[memoryHeapIndex][i] = true;
+    }
+    printInfo();
 }
 
 uint32_t MemoryManager::getBlockCount(uint32_t requiredSize) {
