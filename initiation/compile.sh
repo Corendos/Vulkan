4c3f88013135fa00ac85b56a1933478c3f28930a
godeau_cor <corentin.godeau@supelec.fr>
2018-11-04 21:55:01 +0100

    chore: improved compile script

--- initiation/compile.sh
+++ initiation/compile.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/zsh
 
-$VULKAN_SDK/bin/glslangValidator -V shaders/shader.vert -o shaders/build/vert.spv
-$VULKAN_SDK/bin/glslangValidator -V shaders/shader.frag -o shaders/build/frag.spv
-$VULKAN_SDK/bin/glslangValidator -V shaders/shaderColor.frag -o shaders/build/fragColor.spv
+temp="$(dirname "$0")"
+
+$VULKAN_SDK/bin/glslangValidator -V $temp/shaders/shader.vert -o $temp/shaders/build/vert.spv
+$VULKAN_SDK/bin/glslangValidator -V $temp/shaders/shader.frag -o $temp/shaders/build/frag.spv
+$VULKAN_SDK/bin/glslangValidator -V $temp/shaders/shaderColor.frag -o $temp/shaders/build/fragColor.spv
