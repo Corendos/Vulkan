609b85b04f4742b45057da471abe75b27d7779ee
godeau_cor <corentin.godeau@supelec.fr>
2018-11-12 21:01:27 +0100

    fix: model matrix

--- initiation/src/Transform.cpp
+++ initiation/src/Transform.cpp
@@ -36,8 +36,8 @@ void Transform::rotate(float angle, glm::vec3 axis) {
 
 glm::mat4 Transform::getMatrix() {
     glm::mat4 model(1.0f);
+    model = glm::translate(model, mPosition);
     model *= glm::mat4_cast(mRotationQuaternion);
     model = glm::scale(model, mScale);
-    model = glm::translate(model, mPosition);
     return model;
 }
