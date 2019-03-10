eafa3eec3f6b93947ac76ff1cf3c759820fa4f27
Corendos <corentin.godeau37@gmail.com>
2019-03-10 19:00:36 +0100

    chore: random

--- initiation/compileShaders.sh
+++ initiation/compileShaders.sh
@@ -2,6 +2,5 @@
 
 temp="$(dirname "$0")"
 
-$VULKAN_SDK/bin/glslangValidator -V $temp/shaders/shader.vert -o $temp/shaders/build/vert.spv
-$VULKAN_SDK/bin/glslangValidator -V $temp/shaders/shader.frag -o $temp/shaders/build/frag.spv
-$VULKAN_SDK/bin/glslangValidator -V $temp/shaders/shaderColor.frag -o $temp/shaders/build/fragColor.spv
+$VULKAN_SDK/bin/glslangValidator -V $temp/resources/shaders/shader.vert -o $temp/resources/shaders/build/vert.spv
+$VULKAN_SDK/bin/glslangValidator -V $temp/resources/shaders/shader.frag -o $temp/resources/shaders/build/frag.spv
